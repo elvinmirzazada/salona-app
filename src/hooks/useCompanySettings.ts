61a30750c32bc706ab66487a8350850b1e8addd0
Elvin <elvin.mirzazada@novuter.com>
2026-04-10 23:12:21 +0300

    added max daily booking counts

--- src/hooks/useCompanySettings.ts
+++ src/hooks/useCompanySettings.ts
@@ -10,6 +10,8 @@ interface Company {
   email?: string;
   description?: string;
   timezone?: string;
+  max_daily_booking_count?: number;
+  enforce_sequential_bookings?: number | boolean;
   address?: {
     address?: string;
     city?: string;
